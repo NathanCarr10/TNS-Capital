01fcb45f29c4f3b20a8fa72105fecb8cb2ffa592
Tetiana Urbanovych <tanya.u.2003@gmail.com>
2026-09-15 08:20:08 +0000

    chore: expand seed data adding more entries

--- db/data/02_instruments.sql
+++ db/data/02_instruments.sql
@@ -1,5 +1,12 @@
 -- Instruments seed data
 INSERT INTO instruments (symbol, name, asset_class, currency, tradable) VALUES
-    ('ACME',  'Acme Corp',           'Equity', 'USD', TRUE),
-    ('GLOB',  'Global Growth Fund',  'Fund',   'USD', TRUE),
-    ('BOND1', 'Corporate Bond Fund', 'Bond',   'USD', TRUE);
+    ('ACME',  'Acme Corp',            'Equity', 'USD', TRUE),
+    ('GLOB',  'Global Growth Fund',   'Fund',   'USD', TRUE),
+    ('BOND1', 'Corporate Bond Fund',  'Bond',   'USD', TRUE),
+    ('TECH',  'TechHub Inc',          'Equity', 'USD', TRUE),
+    ('GOOG',  'Google Inc',           'Equity', 'USD', TRUE),
+    ('APPL',  'Apple Inc',            'Equity', 'USD', TRUE),
+    ('MSFT',  'Microsoft Corp',       'Equity', 'USD', TRUE),
+    ('GOLD',  'Gold ETF',             'Commodity', 'USD', TRUE),
+    ('UTIL',  'Utility Sector Fund',  'Fund',   'USD', TRUE),
+    ('HEMP',  'Hemp Industries Fund', 'Fund',   'USD', TRUE);
