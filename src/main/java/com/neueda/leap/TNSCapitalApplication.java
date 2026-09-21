7cc44a55e6a2c3ca97722647ec5f164c32122a1c
Tetiana Urbanovych <tanya.u.2003@gmail.com>
2026-09-21 13:12:06 +0000

    refactor: rename Main.java to match SpringBoot convention

--- src/main/java/com/neueda/leap/TNSCapitalApplication.java
+++ src/main/java/com/neueda/leap/TNSCapitalApplication.java
@@ -1,6 +1,6 @@
 package com.neueda.leap;
 
-public class Main {
+public class TNSCapitalApplication {
     public static void main(String[] args) {
         System.out.println("Hello world from TNS Capital's Sprint 2 project skeleton");
         System.out.println("Testing PR capabilities");
